1ce5660a93b520bdcc6f864ed7614b91c40848fe
dyoni <dyoni5o59192@gmail.com>
2015-10-01 06:44:11 +0000

    railsbasic

---
+++ app/controllers/home_controller.rb
@@ -0,0 +1,22 @@
+class HomeController < ApplicationController
+    def index
+        
+    end
+    
+    def about
+        
+    end
+    
+    def index_1
+        
+    end
+    
+    def index_2
+        
+    end
+    
+    def index_3
+        
+    end
+    
+end
